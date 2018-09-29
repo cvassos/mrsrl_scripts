34b6257d829d64a3add31318f59587744f3358fb
Jonathan <cwvassos@gmail.com>
2018-09-28 17:10:20 -0700

    included updated iperfsweep

--- iperfsweep.py
+++ iperfsweep.py
@@ -1,20 +1,33 @@
 import os
 import re
-import subprocess
+import subprocess as sp
 import csv
 
 #Arrange the options we'd like to sweep over
-win_lens = ['100K', '500K', '1M', '2M']
-parallelargs = ['1','2','5','10']
+win_lens = ['100K', '500K', '1M']
+parallelargs = ['1','2','5']
+Nruns = 4
 outputs = []
 
-for x in win_lens:
-    for y in parallelargs:
-        #Run Iperf & Extract out the speeds from the tests
-        iperf = subprocess.Popen(('iperf','-c','-t20','-O10',''.join(('-w',x)),''.join(('-P',y))),stdout=subprocess.PIPE)
-        iptext = iperf.communicate()
-        matches = re.findall("([0-9]*\.[0-9])(?: Mbits/sec)",iptext)
-        data = [x,y,gp]
-        outputs.append(data)
+filename = "sweepout.csv"
+with open(filename,'wb') as myfile:
+	wr = csv.writer(myfile,quoting=csv.QUOTE_ALL)
+	
+	for i in range(1,Nruns+1):
+		for x in win_lens:
+		    for y in parallelargs:
+			#Run Iperf & Extract out the speeds from the tests
+			if int(y) > 1:
+				iperf = sp.Popen(('iperf','-c192.168.11.143','-t20',''.join(('-w',x)),''.join(('-P',y))),stdout=sp.PIPE)
+				grep = sp.Popen(('grep','SUM'),stdin=iperf.stdout,stdout=sp.PIPE)
+				iperf.stdout.close()
+				outtext = grep.communicate()
+			else:
+				iperf = sp.Popen(('iperf','-c192.168.11.143','-t20',''.join(('-w',x)),''.join(('-P',y))),stdout=sp.PIPE)
+				outtext = iperf.communicate()
+			matches = re.findall("([0-9]*\.?[0-9])(?: Mbits/sec)",outtext[0])
+			data = [x,y,matches[0]]
+			wr.writerow(data)
+			
+			outputs.append(data)
 
-print(outputs)
